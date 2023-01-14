3cc15170e5b94789d86ad5571b80c839f48672b2
Takagi Tasuku <tasuren@outlook.jp>
2023-01-14 13:47:00 +0900

    update: Cacherにて、ipcsからのremoveに加えてupdateに対応。

--- common/cacher.py
+++ common/cacher.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from typing import Any, cast
+from collections.abc import Callable
 
 from ipcs import Client
 
@@ -10,6 +11,10 @@ from ...rextlib.common.cacher import Cacher, KeyT, ValueT
 from ...rextlib.common.utils import Executors
 
 
+__all__ = ("BackendControllableCacher",)
+
+
+_ROUTE_PREFIX = "_cache_of_bc_cacher"
 def _setup_reset_all_cache(self: BackendControllableCacher) -> None:
     global _setup_is_done
     if _setup_is_done:
@@ -22,8 +27,8 @@ def _setup_reset_all_cache(self: BackendControllableCacher) -> None:
         for bc_cacher in _bc_cachers:
             bc_cacher.clear()
 
-    @self.connection.route()
-    async def reset_cache_of_bc_cacher(_) -> None:
+    @self.connection.route(f"reset{_ROUTE_PREFIX}")
+    async def reset(_) -> None:
         await self.connection.loop.run_in_executor(
             self.executors.normal, _reset_cacheof_bc_cachers
         )
@@ -35,7 +40,13 @@ class BackendControllableCacher(Cacher[KeyT, ValueT]):
     connection: Client = cast(Any, None)
     executors: Executors = cast(Any, None)
 
-    def __init__(self, name: str, *args: Any, **kwargs: Any) -> None:
+    def __init__(
+        self, name: str, *args: Any,
+        on_update_by_backend_adjust_value:
+            Callable[[Any], ValueT]
+                = lambda v: cast(ValueT, v),
+        **kwargs: Any
+    ) -> None:
         if self.connection is None:
             raise RuntimeError("IPCSのクライアントのインスタンスが`.connection`に格納されていません。")
         if self.executors is None:
@@ -45,11 +56,21 @@ class BackendControllableCacher(Cacher[KeyT, ValueT]):
 
         super().__init__(*args, **kwargs)
 
-        self.connection.set_route(self._remove_cache, f"remove_{name}_cache_of_bc_cacher")
+        self.connection.set_route(self._remove_cache, f"remove_{name}{_ROUTE_PREFIX}")
+        self.connection.set_route(self._update_cache, f"update_{name}{_ROUTE_PREFIX}")
+        self.on_update_by_backend_adjust_value = on_update_by_backend_adjust_value
+
         _bc_cachers.append(self)
         _setup_reset_all_cache(self)
 
-    def _remove_cache(self, _, key: Any, assassinate: bool = False) -> bool:
+    def _update_cache(self, _, key: KeyT, value: ValueT) -> bool:
+        try:
+            self[key] = self.on_update_by_backend_adjust_value(value)
+        except KeyError:
+            return False
+        return True
+
+    def _remove_cache(self, _, key: KeyT, assassinate: bool = False) -> bool:
         try:
             if assassinate:
                 self.default_on_dead(key, None)
