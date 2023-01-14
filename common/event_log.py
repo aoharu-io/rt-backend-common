d19247d72cc64350d703397d400780b8adf4efd8
Takagi Tasuku <tasuren@outlook.jp>
2023-01-14 14:26:04 +0900

    new: Featureをbot側から移植。

--- common/event_log.py
+++ common/event_log.py
@@ -3,17 +3,27 @@
 from typing import TypeAlias, Literal, TypedDict, get_args
 
 
-__all__ = ("ResultType", "RESULT_TYPES", "PlaceType", "PlaceValue", "Place")
+__all__ = ("ResultType", "RESULT_TYPES", "PlaceType", "PlaceValue", "Place", "Feature")
 
 
 ResultType: TypeAlias = Literal["error", "warning", "success", "unknown"]
-PlaceType: TypeAlias = Literal["guild", "channel", "url", "other"]
-PlaceValue: TypeAlias =  str | None
 RESULT_TYPES: tuple[ResultType, ...]  = get_args(ResultType)
 
+PlaceType: TypeAlias = Literal["guild", "channel", "url", "other"]
+PlaceValue: TypeAlias =  str | None
 
 class Place(TypedDict):
     "イベントログでログの機能が動作した場所を表すデータの型です。"
 
     type: PlaceType
+    "ログの発生源の種類です。"
     value: PlaceValue
+    "場所を割り出すための情報です。例えば`.type`が`guild`の場合、ここにはIDを入れます。"
+
+class Feature(TypedDict):
+    "機能情報のデータの型です。"
+
+    name: str
+    "機能名です。"
+    link: str
+    "機能のダッシュボードへのリンクです。"
