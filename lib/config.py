8e1d8c9db5f8e332714ffd840cdce842b4682e64
Takagi Tasuku <tasuren@outlook.jp>
2023-03-01 17:38:48 +0900

    update: 必要なときだけハッシュ値を生成するように変更。

--- lib/config.py
+++ lib/config.py
@@ -1,6 +1,6 @@
 # RT Lib - Config
 
-__all__ = ("IpcsServer", "get_config_file_path")
+__all__ = ("IpcsServer", "config_file_path", "get_config_file_path")
 
 from typing import TypedDict
 
@@ -16,8 +16,16 @@ class IpcsServer(TypedDict):
     port: int
 
 
+config_file_path = getenv("RT_CONFIG_FILE_PATH", "config.toml")
+REPLACEMENT = "!config_types_hash!"
 def get_config_file_path() -> str:
-    "設定ファイルのパスを取得します。"
-    return getenv("RT_CONFIG_FILE_PATH", "config.toml").replace(
-        "!config_template_hash!", get_file_hash("core/config/types_.py")
-    )
+    """設定ファイルのパスを取得します。
+    環境変数`RT_CONFIG_FILE_PATH`からパスを取得しますが、もしそれが設定されていないのなら、`config.toml`が代わりに返されます。
+    もし、パスに文字列`.REPLACEMENT`が含まれているのなら、それは`core/config/types_.py`のsha256のハッシュ値と置き換えられます。"""
+    global config_file_path
+    if REPLACEMENT in config_file_path:
+        config_file_path = config_file_path.replace(
+            REPLACEMENT,
+            get_file_hash("core/config/types_.py")
+        )
+    return config_file_path
