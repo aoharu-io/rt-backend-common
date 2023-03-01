aeb489c427a0daf8e50ac642e5ea563bfa856acf
Takagi Tasuku <tasuren@outlook.jp>
2023-03-01 15:23:52 +0900

    update: rt-backend-apiのipcs_server.pyが使うものを移植。

---
+++ common/backend.py
@@ -0,0 +1,17 @@
+"RT Lib - バックエンドのためのライブラリです。"
+
+__all__ = ("is_bot", "IPCS_SERVER_ID")
+
+from core.rextlib.common.chiper import ChiperManager
+
+
+IPCS_SERVER_ID = "IPCS-SERVER"
+def is_bot(chiper: ChiperManager, credentials: str) -> bool:
+    "Botかどうかをチェックします。"
+    try:
+        word, _, _ = chiper.decrypt(credentials[3:]).split("_")
+    except Exception:
+        word = ""
+    if word == "RT-Discord-Bot":
+        return True
+    return False
